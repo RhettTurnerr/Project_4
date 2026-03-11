ab686c89fa88755b3f16e2801124545c8f027e20
Rhett Turner <Rhettster06@gmail.com>
2026-03-11 18:38:15 -0500

    backend test 2

--- Project_4_backend/database.js
+++ Project_4_backend/database.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_HOST,
+  port: process.env.PORT,
 });
 
 db.connect((err) => {
